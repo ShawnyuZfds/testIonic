c21284f11dc7cf17906bfbe444a8106412c6dd85
yushang2008 <shawnyuworking@gmail.com>
2017-06-27 19:54:00 -0400

    init

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,5 +1,13 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Component} from "@angular/core";
+import {NavController} from "ionic-angular";
+
+const HEROES = [
+  'Excellent',
+  'Good',
+  'Okay',
+  'Bad',
+  'Terrible'
+]
 
 @Component({
   selector: 'page-about',
@@ -9,6 +17,16 @@ export class AboutPage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  answers = HEROES;
+  selectedAnswer: '';
+  currentPage = 1;
+  totalPage = 4;
+  question = "How would you rate your chicken shawama sandwich?";
+
+  onSelect(answer): void {
+    this.selectedAnswer = answer;
   }
 
 }
