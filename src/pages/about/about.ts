1cc00090ca71b66f7d65c74f2e68eaa5aa75027c
yushang2008 <shawnyuworking@gmail.com>
2017-06-27 20:35:09 -0400

    init

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,12 +1,29 @@
 import {Component} from "@angular/core";
 import {NavController} from "ionic-angular";
 
-const HEROES = [
-  'Excellent',
-  'Good',
-  'Okay',
-  'Bad',
-  'Terrible'
+const questions = [
+  {
+    question: "How would you rate your chicken shawama sandwich?",
+    answers: ['Excellent',
+      'Good',
+      'Okay',
+      'Bad',
+      'Terrible']
+  },
+  {
+    question: "2How would you rate your chicken shawama sandwich?",
+    answers: ['Excellent',
+      'Good',
+      'Okay',
+      'Bad',
+      'Terrible',
+      'Excellen1t',
+      'Go2od',
+      'Ok3ay',
+      'B2ad',
+      'Ter1rible']
+  }
+
 ]
 
 @Component({
@@ -19,14 +36,26 @@ export class AboutPage {
 
   }
 
-  answers = HEROES;
-  selectedAnswer: '';
+  selectedAnswer = '';
+  totalPage = questions.length;
+  currentQues = 0;
   currentPage = 1;
-  totalPage = 4;
-  question = "How would you rate your chicken shawama sandwich?";
+  answers = questions[0].answers;
+  question = questions[0].question;
+  userAnswers = [];
 
   onSelect(answer): void {
     this.selectedAnswer = answer;
+    this.userAnswers.push(answer);
   }
 
+  nextQuestion(): void {
+    if (this.currentPage < questions.length && this.userAnswers[this.currentQues]) {
+      this.currentQues++;
+      this.currentPage++;
+      this.answers = questions[this.currentQues].answers;
+      this.question = questions[this.currentQues].question;
+      this.selectedAnswer = '';
+    }
+  }
 }
