0d8dbd0c6e78000d9f9eb606d55bb488d9cde8bc
yushang2008 <shawnyuworking@gmail.com>
2017-06-28 23:41:46 -0400

    init

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,7 +1,7 @@
 import {Component} from "@angular/core";
 import {NavController} from "ionic-angular";
 import {BarcodeScanner} from "@ionic-native/barcode-scanner";
-
+import { AboutPage } from '../about/about';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -13,8 +13,13 @@ export class HomePage {
   }
 
   start_barcodescanner() {
+
     this.barcodeScanner.scan().then((barcodeData) => {
       // Success! Barcode data is here
+      this.navCtrl.setRoot(AboutPage, {}, {
+        animate: true,
+        direction: 'forward'
+      });
     }, (err) => {
       // An error occurred
     });
